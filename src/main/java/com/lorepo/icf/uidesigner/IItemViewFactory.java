e68b3aa62a741e608510b276976fa6711d64f63a
mmotlawski <miotla007@wp.pl>
2016-03-10 14:19:54 +0100

    Changed interface for calculating max score

--- src/main/java/com/lorepo/icf/uidesigner/IItemViewFactory.java
+++ src/main/java/com/lorepo/icf/uidesigner/IItemViewFactory.java
@@ -1,7 +1,7 @@
 package com.lorepo.icf.uidesigner;
 
 
-public interface IItemViewFactory<T> {
+public interface IItemViewFactory<T, M> {
 
-	public ItemView<T> getWidget(T item);
+	public ItemView<T> getWidget(T item, M calculateMaxScore);
 }
--- src/main/java/com/lorepo/icf/uidesigner/ItemViewFactory.java
+++ src/main/java/com/lorepo/icf/uidesigner/ItemViewFactory.java
@@ -2,15 +2,14 @@ package com.lorepo.icf.uidesigner;
 
 import com.google.gwt.user.client.ui.HTML;
 
-public class ItemViewFactory<T> implements IItemViewFactory<T> {
+public class ItemViewFactory<T, M> implements IItemViewFactory<T,M> {
 
 	@Override
-	public ItemView<T> getWidget(T item) {
+	public ItemView<T> getWidget(T item, M calculateMaxScore) {
 		
 		ItemView<T> widget = new ItemView<T>(item);
 		widget.setInnerWidget(new HTML(item.toString()));
 		
 		return widget;
 	}
-
 }
