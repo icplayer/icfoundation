a3ae4c398b9e846480cd35ad9b4c309cb1121436
Tomasz Krassowski <tomasz.krassowski@solwit.com>
2018-11-15 16:11:26 +0100

    Rozszerzenie JSONUtils

--- src/main/java/com/lorepo/icf/utils/JSONUtils.java
+++ src/main/java/com/lorepo/icf/utils/JSONUtils.java
@@ -2,14 +2,17 @@ package com.lorepo.icf.utils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONBoolean;
+import com.google.gwt.json.client.JSONNumber;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.json.client.JSONString;
 import com.google.gwt.json.client.JSONValue;
+
 
 public class JSONUtils {
 	
@@ -28,26 +31,6 @@ public class JSONUtils {
 		return json.toString();
 	}
 
-	
-	public static HashMap<String,String> decodeHashMap(String jsonText){
-		
-		HashMap<String,String> output = new HashMap<String, String>();
-		JSONValue jsonValue = JSONParser.parseStrict(jsonText);
-		
-		if(jsonValue instanceof JSONObject){
-			JSONObject json = (JSONObject) jsonValue;
-			for(String key : json.keySet()){
-				if(json.get(key).isString() != null){
-					String value = json.get(key).isString().stringValue();
-					output.put(key, value);
-				}
-			}
-		}
-		
-		return output;
-	}
-
-
 	public static String toJSONString(ArrayList<Boolean> list) {
 
 		JSONArray json = new JSONArray();
@@ -59,7 +42,40 @@ public class JSONUtils {
 		
 		return json.toString();
 	}
+	
+	public static String toJSONString(List<JSONValueAdapter> list) {
 
+		JSONArray json = new JSONArray();
+		
+		for(int i = 0; i < list.size(); i++){
+			JSONValue jsonValue = list.get(i).getValue();
+			json.set(i, jsonValue); 
+		}
+		
+		return json.toString();
+	}
+	
+	public static ArrayList<Integer> decodeIntegerArray(String jsonText) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		JSONValue jsonValue = JSONParser.parseStrict(jsonText);
+		JSONArray array = jsonValue.isArray();
+		
+		if (array == null) {
+			return list;
+		}
+		
+		for (int i = 0; i < array.size(); i++) {
+			JSONNumber number = array.isNumber();
+			
+			if (number != null) {
+				Integer value = new Double(number.doubleValue()).intValue();
+				list.add(value);
+			}
+		}
+		
+		return list;
+	}
 
 	public static ArrayList<Boolean> decodeArray(String jsonText) {
 
@@ -78,6 +94,24 @@ public class JSONUtils {
 		}
 		
 		return list;
+	}
+	
+	public static HashMap<String,String> decodeHashMap(String jsonText){
+		
+		HashMap<String,String> output = new HashMap<String, String>();
+		JSONValue jsonValue = JSONParser.parseStrict(jsonText);
+		
+		if(jsonValue instanceof JSONObject){
+			JSONObject json = (JSONObject) jsonValue;
+			for(String key : json.keySet()){
+				if(json.get(key).isString() != null){
+					String value = json.get(key).isString().stringValue();
+					output.put(key, value);
+				}
+			}
+		}
+		
+		return output;
 	}
 	
 	public static native <T> T parseOverlayType(JavaScriptObject obj, Class<T> type) /*-{
