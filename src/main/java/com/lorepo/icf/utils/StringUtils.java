0b51a2e48ed56e6fe1a2af82e7a7225d31dfada3
msawoniuk <79645404+msawoniuk@users.noreply.github.com>
2024-02-16 12:45:39 +0100

    Fix images not loading in IOS when added as inner HTML, re #9106 (#30)

--- src/main/java/com/lorepo/icf/utils/StringUtils.java
+++ src/main/java/com/lorepo/icf/utils/StringUtils.java
@@ -218,7 +218,13 @@ public class StringUtils {
 	 */
 	public static String updateLinks(String xml, String baseURL) {
 
-		if(baseURL == null || (!baseURL.startsWith("/") && !baseURL.startsWith("http") && !baseURL.startsWith("file"))){
+		if (baseURL == null
+			|| (!baseURL.startsWith("/")
+				&& !baseURL.startsWith("http")
+				&& !baseURL.startsWith("file")
+				&& !baseURL.startsWith("capacitor")
+			)
+		){
 			return xml;
 		}
 		
@@ -242,7 +248,13 @@ public class StringUtils {
 			index = input.indexOf(ch);
 			String url = input.substring(0, index);
 			input = input.substring(index);
-			if(url.startsWith("#") || url.startsWith("/") || url.startsWith("http") || url.startsWith("file") || url.startsWith("data:")){
+			if (url.startsWith("#")
+				|| url.startsWith("/")
+				|| url.startsWith("http")
+				|| url.startsWith("file")
+				|| url.startsWith("data:")
+				|| url.startsWith("capacitor")
+			){
 				output += url;
 			}
 			else{
@@ -269,8 +281,12 @@ public class StringUtils {
 			index = input.indexOf(ch);
 			String url = input.substring(0, index);
 			input = input.substring(index);
-			if(url.startsWith("#") || url.startsWith("/") || url.startsWith("http") 
-					|| url.startsWith("file") || url.startsWith("javascript")){
+			if (url.startsWith("#")
+				|| url.startsWith("/")
+				|| url.startsWith("http")
+				|| url.startsWith("file")
+				|| url.startsWith("javascript")
+            ){
 				output += url;
 			}
 			else{
